49bd78df9e0ea5d28d108acf418931325f6ad854
norita <marnori2001@gmail.com>
2024-06-16 20:00:47 -0300

    Chore: add nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CatsController } from './controllers/cats.controller';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  controllers: [CatsController],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
