0324c1af424260ab5ba0781611797bbf8f359a92
norita <marnori2001@gmail.com>
2024-06-23 00:27:53 -0300

    feat: added auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+
 import { UsersModule } from './users/users.module';
 import { dataBaseConfig } from './database/database.config';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { UsersController } from './users/users.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [SequelizeModule.forRoot(dataBaseConfig), UsersModule],
+  imports: [SequelizeModule.forRoot(dataBaseConfig), UsersModule, AuthModule],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  sigIn(@Body() signInDto: Record<string, any>) {
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() request: any) {
+    return request.user;
+  }
+}
